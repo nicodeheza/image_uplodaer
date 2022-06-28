48122e1f01f622222dceedd18ef2d2031ce434be
nicodeheza <nicolasdeheza@hotmail.com>
2022-06-28 20:11:41 -0300

    first commit, config and some boilerplate

---
+++ components/ImageCard.tsx
@@ -0,0 +1,7 @@
+export default function ImageCard() {
+	return (
+		<div>
+			<div></div>
+		</div>
+	);
+}
---
+++ components/Uploader.tsx
@@ -0,0 +1,7 @@
+export default function Uploader() {
+	return (
+		<div data-testid="Uploader_container" style={{maxWidth: "402px", maxHeight: "469px"}}>
+			<div></div>
+		</div>
+	);
+}
