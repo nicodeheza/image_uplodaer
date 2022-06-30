44d06df26ad747c2fe3fe7261fb03c3fdcd2eec7
nicodeheza <nicolasdeheza@hotmail.com>
2022-06-30 19:29:25 -0300

    fix link crossOrigin

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
 		<Html>
 			<Head>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 				<link
 					href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@500&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
 					rel="stylesheet"
